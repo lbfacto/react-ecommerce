1fe4d6a5cbeea86098de58bf39842b7b849e9be9
lbfacto <papalayeba03@gmail.com>
2023-07-13 03:36:15 +0000

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
+import React from 'react'
+
 import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
+export default MyApp
